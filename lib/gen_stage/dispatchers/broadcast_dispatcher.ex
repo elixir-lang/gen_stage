ca131db580cae8d815ab0c317a0915eb2137020d
José Valim <jose.valim@plataformatec.com.br>
2018-11-27 09:11:10 +0100

    Remove empty new line from docs

--- lib/gen_stage/dispatchers/broadcast_dispatcher.ex
+++ lib/gen_stage/dispatchers/broadcast_dispatcher.ex
@@ -2,7 +2,7 @@ defmodule GenStage.BroadcastDispatcher do
   @moduledoc """
   A dispatcher that accumulates demand from all consumers
   before broadcasting events to all of them.
-  
+
   This dispatcher guarantees that events are dispatched to all
   consumers without exceeding the demand of any given consumer.
 
